39e106de8f659b3494018e768755d53b44096873
NirmalAriyathilake <nirmalnavitas@gmail.com>
2018-09-30 23:06:04 +0530

    unicode

--- data_load.py
+++ data_load.py
@@ -51,6 +51,7 @@ def load_data(mode="train"):
                 fpaths.append(fpath)
                 # text = "{}".format(text.encode('utf-8'))
                 text = text.encode("utf-8")
+                text = unicode(text, "utf-8")
                 print(text)
                 text = text_normalize(text) + "E"  # E: EOS
                 print(text)
